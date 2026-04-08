cd5bc8759faabb303825a890ca55d8f82caeca82
mkomi <mkomissarov207@gmail.com>
2026-04-08 22:12:02 +0300

    ssl cert

---
+++ nginx/entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+mkdir -p /etc/nginx/ssl
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/key.pem -out /etc/nginx/ssl/cert.pem -subj '/CN=localhost/O=Transport/C=RU'
+nginx -g 'daemon off;'
